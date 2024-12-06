7aba5b30fb76fb712ccc389721a556f61b1f1799
kayes360 <42603790+kayes360@users.noreply.github.com>
2024-12-06 22:26:07 +0600

    api patch error case added

--- app/api/videos/[id]/route.js
+++ app/api/videos/[id]/route.js
@@ -8,13 +8,21 @@ export async function GET(_request, {params}) {
 } 
 
 export async function PATCH(request, {params}) {
-    const videoUpdateData = await request.json()
+    const videoUpdateData = await request.json();
+    
+  
     const videoId = params?.id 
     const videoIndex = videos.findIndex((video) => video.videoId === videoId)
 
     videos[videoIndex].title =videoUpdateData.title
     videos[videoIndex].description =videoUpdateData.description
-    
+    if ( Object.keys(videoUpdateData).some(key => key !== 'title' && key !== 'description')) {
+        const invalidKeys = Object.keys(videoUpdateData).filter(key => key !== 'title' && key !== 'description');
+        return NextResponse.json(
+            { error: `Only 'title' and 'description' can be updated. Invalid keys: ${invalidKeys.join(', ')}` },
+            { status: 400 }
+        );
+      }
 
     return NextResponse.json(videos[videoIndex])
 } 
